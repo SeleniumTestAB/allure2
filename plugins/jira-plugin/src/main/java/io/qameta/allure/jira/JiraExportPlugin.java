1f568e992928aa111e18f340da22a8c43b7d430b
Arkadiusz <karas.tsukasa@gmail.com>
2020-04-25 21:08:05 +0200

    refactor of Jira plugin

--- plugins/jira-plugin/src/main/java/io/qameta/allure/jira/JiraExportPlugin.java
+++ plugins/jira-plugin/src/main/java/io/qameta/allure/jira/JiraExportPlugin.java
@@ -23,7 +23,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.nio.file.Path;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
